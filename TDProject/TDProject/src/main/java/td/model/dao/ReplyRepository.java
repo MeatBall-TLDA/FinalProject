b96b9e5f17dafb2a69ecc019720d8e4df7bbc794
youngwookjo <wwwe2410@naver.com>
2019-12-20 14:22:23 +0900

    add : css 수정 및 세션준비

---
+++ TDProject/TDProject/src/main/java/td/model/dao/ReplyRepository.java
@@ -0,0 +1,10 @@
+package td.model.dao;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import td.model.domain.ReplyDTO;
+
+public interface ReplyRepository extends ElasticsearchRepository<ReplyDTO, String> {
+
+	ReplyDTO findByUserIdAndRepBoardId(String userId, String repBoardId);
+}
---
+++ TDProject/TDProject/src/main/java/td/model/domain/HiddenBoardDTO.java
@@ -0,0 +1,31 @@
+package td.model.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@Document(indexName = "hidden_board", type = "_doc")
+public class HiddenBoardDTO {
+	  @Id
+	  private String id;
+	  private String contents;
+	  private String hashtag;
+	  private String postingDate;
+	  private String openDate;
+	  private Integer heart;
+	  private Integer claim;
+	  private String nickname;
+	  private String category;
+	  
+	  public HiddenBoardDTO(String hashtag) {
+			super();
+			this.hashtag = hashtag;
+		}
+}
+
